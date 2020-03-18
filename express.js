c67650d40f92081d4c24be29ed323e2e8d10864a
Gabe Young <gabriel_young@emerson.edu>
2020-03-18 15:13:50 -0400

    reorganized GET and 1st attempt at POST

--- express.js
+++ express.js
@@ -9,24 +9,27 @@ const app = express();
 //Server Route
 //Reminder: Ask Alper about the weird slow down when loading the /notes page. It stalls while loading. However if you cancel the loading process halfway through the page will load like normal. WHYYYYY??????
 
-app.get("/notes", (req, res) =>{
-    res.sendFile(path.join(__dirname + `/Develop/public/notes.html`));
+app.post(`/api/notes`, (req, res) => {
+    fs.readFile(`${__dirname}/Develop/db/db.sjon`, (err, data) =>{
+        if(err) throw err;
+        console.log(data);
+    });
 });
-
-app.get("/api/notes", (req, res) => {
-    const file = (__dirname + `/Develop/db/db.json`);
-    const data = fs.readFileSync(file, `utf8`) 
-    return res.json(data);  
-  });
 
 app.get("*", (req, res) =>{
     if(req.url === "/"){
-    res.sendFile(path.join(__dirname + `/Develop/public/index.html`));
+        res.sendFile(path.join(__dirname + `/Develop/public/index.html`));
+    } else if (req.url === `/notes`){
+        res.sendFile(path.join(__dirname + `/Develop/public/notes.html`));
+    } else if (req.url === `/api/notes`){
+        const file = (__dirname + `/Develop/db/db.json`);
+        const data = fs.readFileSync(file, `utf8`) 
+        return res.json(data);
     }
 });
 
 app.listen(PORT, () =>{
     console.log(`Server listening on: http://localhost:${PORT}`);
-})
+});
 
 
